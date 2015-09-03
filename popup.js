3ef6d3e27034704472aa5999891756373dc75be0
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-03 21:36:49 +0200

    Google domain is excluded

--- popup.js
+++ popup.js
@@ -1,9 +1,9 @@
 var searchBaseUrl = "http://webcache.googleusercontent.com/search?q=cache%3A";
 var currentUrl = window.location.href;
+//Ou ajouter le webcache dans le manifest
 if (!currentUrl.includes(searchBaseUrl)) {
   var cacheViewUrl = searchBaseUrl + encodeURIComponent(currentUrl);
   chrome.extension.sendRequest({
     redirect: cacheViewUrl
   })
-
 }
