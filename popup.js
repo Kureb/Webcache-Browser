34dfea77a850e54939a9cce0d3d0bab64a0991bf
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-03 22:04:40 +0200

    Checks if the cached version is not a 404 error before making the redirection

--- popup.js
+++ popup.js
@@ -1,9 +1,17 @@
 var searchBaseUrl = "http://webcache.googleusercontent.com/search?q=cache%3A";
 var currentUrl = window.location.href;
-//Ou ajouter le webcache dans le manifest
-if (!currentUrl.includes(searchBaseUrl)) {
-  var cacheViewUrl = searchBaseUrl + encodeURIComponent(currentUrl);
-  chrome.runtime.sendMessage({
-    redirect: cacheViewUrl
-  })
-}
+var cacheViewUrl = searchBaseUrl + encodeURIComponent(currentUrl);
+//Avant d'envoyer la requête, vérifier qu'on tombe pas sur l'erreur
+//404 du cache google
+$.ajax(cacheViewUrl,
+   {
+      statusCode: {
+      404: function() {
+         alert('page not found');
+      }
+   }, success: function() {
+     chrome.runtime.sendMessage({
+       redirect: cacheViewUrl
+     })
+   }
+});
