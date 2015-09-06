d95e8397836af6550d02646f3cc26554f6ee5077
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-06 12:20:40 +0200

    Adds a information popup when cache is unavailable for the current page

--- popup.js
+++ popup.js
@@ -1,19 +1,24 @@
-var searchBaseUrl = "https://webcache.googleusercontent.com/search?q=cache%3A";
+var searchBaseUrl = "http://webcache.googleusercontent.com/search?q=cache%3A";
 var currentUrl = window.location.href;
 var fullVersion = '&strip=0&vwsrc=0';
 var cacheViewUrl = searchBaseUrl + encodeURIComponent(currentUrl) + fullVersion;
-//Before making the redirection, check if we will not
-//the request come back with no status code (404..)
-//TODO : si pas success
-$.ajax(cacheViewUrl,
-  {
-    success: function() {
-      chrome.runtime.sendMessage({
-        redirect: cacheViewUrl
-      })
-    }
-    /*,
-    error: function() {
-      alert("Cache unavailable");
-    }*/
+
+/* Before making the redirection, check if
+* the request come back with no status code (404..)
+* Doing a HEAD request instead of the default GET will only returns the headers
+* and indicates whether the page exists (reponse codes 200 - 299 ) or not
+* (response codes 400 - 499)
+*/
+$.ajax({
+  type: 'HEAD',
+  url: cacheViewUrl,
+  success: function() {
+    chrome.runtime.sendMessage({
+      redirect: cacheViewUrl
+    })
+  },
+  error: function() {
+    // page does not exist
+    $.notify("Sorry, no cache is available for this page.", "warn", { clickToHide: true, autoHide: false });
+  }
 });
