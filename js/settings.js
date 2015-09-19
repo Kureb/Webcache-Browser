eaeeb38f028ad8a70b99ba0449bd13a53087d252
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-19 23:52:29 +0200

    Missing letter

--- js/settings.js
+++ js/settings.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function(){
   var i18nTitle = chrome.i18n.getMessage("extName");
   document.getElementById("title").innerHTML = i18nTitle;
 
-  var i18nCheckBoxCacheBrowser = chrome.i18n.getMessage("checkBoxCacheBrosing");
+  var i18nCheckBoxCacheBrowser = chrome.i18n.getMessage("checkBoxCacheBrowsing");
   document.getElementById("enable_disable").innerHTML = i18nCheckBoxCacheBrowser;
 
     var input = document.getElementById('cache-browser');
