c352af0ba78455c4744bc3451e6c517b2980ec81
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-19 22:50:55 +0200

    Clean context menu js

--- js/menu.js
+++ js/menu.js
@@ -2,14 +2,9 @@ chrome.contextMenus.create({
   "type": "normal",
   "title": "See cached version",
   "contexts": ["all", "page", "frame", "selection", "link", "editable", "image", "video", "audio"],
-  "onclick": function (info, tab) {
-    chrome.tabs.query({
-      "active": true,
-      "currentWindow": true
-    }, function (tabs) {
-      chrome.tabs.sendMessage(tabs[0].id, {
+  "onclick": function (tab) {
+      chrome.tabs.sendMessage(tab.id, {
         "functiontoInvoke": "cache"
       });
-    });
   }
 });
