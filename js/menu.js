6e82437ce77e169a93c77e33cd13320613b51a2a
Alexandre DAUSSY <alexandre.daussy@gmail.com>
2015-09-19 23:05:46 +0200

    Fix missing parameter

--- js/menu.js
+++ js/menu.js
@@ -2,7 +2,7 @@ chrome.contextMenus.create({
   "type": "normal",
   "title": "See cached version",
   "contexts": ["all", "page", "frame", "selection", "link", "editable", "image", "video", "audio"],
-  "onclick": function (tab) {
+  "onclick": function (info, tab) {
       chrome.tabs.sendMessage(tab.id, {
         "functiontoInvoke": "cache"
       });
